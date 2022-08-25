08668c177495478aa70da3c3a800d55feb6d356c
Kaguyo <gabriel.silverio.silvaaa@gmail.com>
2022-08-24 22:09:36 -0300

    pratica 1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,5 @@
-let nome
-let email 
-let profissao 
-let idade 
-let lugar 
+let nome = "Gabriel Silverio da Silva"
+let email = "gabriel.silverio.silvaaa@gmail.com"
+let profissao = "Desenvolvedor"
+let idade = 17
+let lugar = "EUA"
